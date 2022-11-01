821f2db067caea14a8150470b3d29220ccc30bd9
BrunoAmbricca <bambricca@gmail.com>
2022-11-01 14:09:20 -0300

    Update config.js

--- config.js
+++ config.js
@@ -2,10 +2,6 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-//console.log(process.env.PORT)
-//console.log(process.env.STRCNX)
-//console.log(process.env.MODO_PERSISTENCIA)
-
 const PORT = 8080
 const STRCNX = process.env.STRCNX || 'mongodb://localhost'
 const MODO_PERSISTENCIA = process.env.MODO_PERSISTENCIA || 'MEM'   // MEM - MONGO
